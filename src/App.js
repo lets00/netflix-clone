23394eed4b436ee5f1e2e0fdcc2d8f231a0a4430
Luís Eduardo <eduardovansilva@gmail.com>
2020-09-08 21:17:59 -0300

    Add default lists

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import tmdb from './tmdb'
+import MovieRow from './components/MovieRow';
+import './App.css'
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      const list = await tmdb.getHomeList()
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return (
-    <div>
-      
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
